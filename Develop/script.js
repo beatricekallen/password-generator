ca6c243410ecb95c543df23e7985450d4237b28b
beatricekallen <bkallen90@gmail.com>
2022-02-24 11:21:25 -0500

    fixes bug so password is generated on button click

--- Develop/script.js
+++ Develop/script.js
@@ -83,4 +83,4 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword());
+generateBtn.addEventListener("click", writePassword);
