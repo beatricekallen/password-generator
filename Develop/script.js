0d981583513f8fecf194efea7178c8b8ceca24e8
beatricekallen <bkallen90@gmail.com>
2022-02-24 11:15:32 -0500

    adds return password to window functionality

--- Develop/script.js
+++ Develop/script.js
@@ -1,202 +1,82 @@
 // Assignment code here
-
-/* 
-Pseudocode
-
-Clicking button runs password generation function
-Prompt for length of password (8-128 characters)
-Prompt for lowercase
-Prompt for uppercase
-Prompt for numeric
-Prompt for special characters
-At least one must be chosen
-When all prompts answered, password is generated
-Generated password is shown in an alert or displayed on the page
-*/
-
-var promptLength = window.prompt(
-  "How long would you like your password to be? Please choose a number from 8 to 128."
-);
-if (
-  promptLength === "" ||
-  promptLength === null ||
-  promptLength < 8 ||
-  promptLength > 128
-) {
-  window.alert(
-    "Your selection is invalid. Please choose a number from 8 to 128."
+function generatePassword() {
+  var promptLength = window.prompt(
+    "How long would you like your password to be? Please choose a number from 8 to 128."
   );
-} else {
-  promptLength = parseInt(promptLength);
-}
-
-var promptUpper = window.prompt(
-  "Would you like to include uppercase letters? Please enter 'Yes' or 'No.'"
-);
-if (promptUpper === "" || promptUpper === null) {
-  window.alert("Your selection is invalid. Please answer 'Yes' or 'No.'");
-}
-// else if (promptUpper === "Yes" || promptUpper === "yes") {
-//   return promptUpper;
-// }
-
-var promptLower = window.prompt(
-  "Would you like to include lowercase letters? Please enter 'Yes' or 'No.'"
-);
-if (promptLower === "" || promptLower === null) {
-  window.alert("Your selection is invalid. Please answer 'Yes' or 'No.'");
-}
-// else if (promptLower === "Yes" || promptLower === "yes") {
-//   return promptLower;
-// }
-
-var promptNum = window.prompt(
-  "Would you like to include numbers? Please enter 'Yes' or 'No.'"
-);
-if (promptNum === "" || promptNum === null) {
-  window.alert("Your selection is invalid. Please answer 'Yes' or 'No.'");
-}
-// else if (promptNum === "Yes" || promptNum === "yes") {
-//   return promptNum;
-// }
-
-var promptSpecial = window.prompt(
-  "Would you like to include special characters? Please enter 'Yes' or 'No.'"
-);
-if (promptSpecial === "" || promptSpecial === null) {
-  window.alert("Your selection is invalid. Please answer 'Yes' or 'No.'");
-}
-// else if (promptSpecial === "Yes" || promptSpecial === "yes") {
-//   return promptSpecial;
-// }
-
-function generatePassword(length) {
-  var password = "";
   if (
-    promptUpper === "Yes" &&
-    promptLower === "Yes" &&
-    promptNum === "Yes" &&
-    promptSpecial === "Yes"
+    promptLength === "" ||
+    promptLength === null ||
+    promptLength < 8 ||
+    promptLength > 128
   ) {
-    var characters =
-      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123456789!@#$%^&*";
-  } else if (
-    promptUpper === "Yes" &&
-    promptLower === "Yes" &&
-    promptNum === "Yes" &&
-    promptSpecial === "No"
-  ) {
-    var characters =
-      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123456789";
-  } else if (
-    promptUpper === "Yes" &&
-    promptLower === "Yes" &&
-    promptNum === "No" &&
-    promptSpecial === "Yes"
-  ) {
-    var characters =
-      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!@#$%^&*";
-  } else if (
-    promptUpper === "No" &&
-    promptLower === "Yes" &&
-    promptNum === "Yes" &&
-    promptSpecial === "Yes"
-  ) {
-    var characters = "abcdefghijklmnopqrstuvwxyz123456789!@#$%^&*";
-  } else if (
-    promptUpper === "Yes" &&
-    promptLower === "No" &&
-    promptNum === "Yes" &&
-    promptSpecial === "Yes"
-  ) {
-    var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789!@#$%^&*";
-  } else if (
-    promptUpper === "Yes" &&
-    promptLower === "No" &&
-    promptNum === "Yes" &&
-    promptSpecial === "No"
-  ) {
-    var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789";
-  } else if (
-    promptUpper === "Yes" &&
-    promptLower === "Yes" &&
-    promptNum === "No" &&
-    promptSpecial === "No"
-  ) {
-    var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-  } else if (
-    promptUpper === "Yes" &&
-    promptLower === "No" &&
-    promptNum === "No" &&
-    promptSpecial === "Yes"
-  ) {
-    var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*";
-  } else if (
-    promptUpper === "No" &&
-    promptLower === "Yes" &&
-    promptNum === "Yes" &&
-    promptSpecial === "No"
-  ) {
-    var characters = "abcdefghijklmnopqrstuvwxyz123456789";
-  } else if (
-    promptUpper === "No" &&
-    promptLower === "Yes" &&
-    promptNum === "No" &&
-    promptSpecial === "Yes"
-  ) {
-    var characters = "abcdefghijklmnopqrstuvwxyz!@#$%^&*";
-  } else if (
-    promptUpper === "No" &&
-    promptLower === "No" &&
-    promptNum === "Yes" &&
-    promptSpecial === "Yes"
-  ) {
-    var characters = "123456789!@#$%^&*";
-  } else if (
-    promptUpper === "Yes" &&
-    promptLower === "No" &&
-    promptNum === "No" &&
-    promptSpecial === "No"
-  ) {
-    var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  } else if (
-    promptUpper === "No" &&
-    promptLower === "Yes" &&
-    promptNum === "No" &&
-    promptSpecial === "No"
-  ) {
-    var characters = "abcdefghijklmnopqrstuvwxyz";
-  } else if (
-    promptUpper === "No" &&
-    promptLower === "No" &&
-    promptNum === "No" &&
-    promptSpecial === "Yes"
-  ) {
-    var characters = "!@#$%^&*";
-  } else if (
-    promptUpper === "No" &&
-    promptLower === "No" &&
-    promptNum === "Yes" &&
-    promptSpecial === "No"
-  ) {
-    var characters = "123456789";
+    window.alert(
+      "Your selection is invalid. Please choose a number from 8 to 128."
+    );
+  } else {
+    promptLength = parseInt(promptLength);
   }
 
-  var charactersLength = characters.length;
-  for (var i = 0; i < length; i++) {
-    password += characters.charAt(Math.floor(Math.random() * charactersLength));
+  var promptUpper = window.confirm("Click OK to include uppercase letters.");
+  var promptLower = window.confirm("Click OK to include lowercase letters.");
+  var promptNum = window.confirm("Click OK to include numbers.");
+  var promptSpecial = window.confirm("Click OK to include special characters.");
+
+  function makePassword(length) {
+    var password = "";
+    if (promptUpper && promptLower && promptNum && promptSpecial) {
+      var characters =
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123456789!@#$%^&*";
+    } else if (promptUpper && promptLower && promptNum && !promptSpecial) {
+      var characters =
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123456789";
+    } else if (promptUpper && promptLower && !promptNum && promptSpecial) {
+      var characters =
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!@#$%^&*";
+    } else if (!promptUpper && promptLower && promptNum && promptSpecial) {
+      var characters = "abcdefghijklmnopqrstuvwxyz123456789!@#$%^&*";
+    } else if (promptUpper && !promptLower && promptNum && promptSpecial) {
+      var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789!@#$%^&*";
+    } else if (promptUpper && !promptLower && promptNum && !promptSpecial) {
+      var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789";
+    } else if (promptUpper && promptLower && !promptNum && !promptSpecial) {
+      var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    } else if (promptUpper && !promptLower && !promptNum && promptSpecial) {
+      var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*";
+    } else if (!promptUpper && promptLower && promptNum && !promptSpecial) {
+      var characters = "abcdefghijklmnopqrstuvwxyz123456789";
+    } else if (!promptUpper && promptLower && !promptNum && promptSpecial) {
+      var characters = "abcdefghijklmnopqrstuvwxyz!@#$%^&*";
+    } else if (!promptUpper && !promptLower && promptNum && promptSpecial) {
+      var characters = "123456789!@#$%^&*";
+    } else if (promptUpper && !promptLower && !promptNum && !promptSpecial) {
+      var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    } else if (!promptUpper && promptLower && !promptNum && !promptSpecial) {
+      var characters = "abcdefghijklmnopqrstuvwxyz";
+    } else if (!promptUpper && !promptLower && !promptNum && promptSpecial) {
+      var characters = "!@#$%^&*";
+    } else if (!promptUpper && !promptLower && promptNum && !promptSpecial) {
+      var characters = "123456789";
+    } else {
+      window.alert("You must select at least one criteria. Please try again.");
+    }
+
+    var charactersLength = characters.length;
+    for (var i = 0; i < length; i++) {
+      password += characters.charAt(
+        Math.floor(Math.random() * charactersLength)
+      );
+    }
+    return password;
   }
-  return password;
+  return makePassword(promptLength);
 }
-
-console.log(generatePassword(promptLength));
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword(promptLength);
+  var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
